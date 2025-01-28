891c1babbd0e7846ed812fefa33fcad34a233be7
Cameron Maxwell <cameronwmax@icloud.com>
2025-01-28 15:42:11 -0800

    Create constants folder, Create buttons.js file, Move code

--- src/App.js
+++ src/App.js
@@ -2,29 +2,8 @@ import "./App.css";
 import Buttons from "./components/Buttons";
 import Calculator from "./components/Calculator";
 import Screen from "./components/Screen";
+import { buttons } from "./constants/buttons";
 import useCalculator from "./hooks/useCalculator";
-
-const buttons = [
-  { label: "C", type: "clear" },
-  { label: "+-", type: "toggle-sign" },
-  { label: "%", type: "percentage" },
-  { label: "÷", type: "operator/divide" },
-  { label: "7", type: "number" },
-  { label: "8", type: "number" },
-  { label: "9", type: "number" },
-  { label: "x", type: "operator/multiply" },
-  { label: "4", type: "number" },
-  { label: "5", type: "number" },
-  { label: "6", type: "number" },
-  { label: "-", type: "operator/subtract" },
-  { label: "1", type: "number" },
-  { label: "2", type: "number" },
-  { label: "3", type: "number" },
-  { label: "+", type: "operator/add" },
-  { label: "0", type: "number" },
-  { label: ".", type: "decimal" },
-  { label: "=", type: "equal" },
-];
 
 function App() {
   const { input, equation, isError, handleClick, handleClear } = useCalculator();
